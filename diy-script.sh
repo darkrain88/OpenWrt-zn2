a06d4ca838aef6dc8eaa8c09d630ccd7a869f98a
darkrain88 <39786175+darkrain88@users.noreply.github.com>
2024-04-01 12:41:16 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -74,6 +74,8 @@ git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
 # MosDNS
 git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
 
+# Adguardhome
+git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 # Alist
 git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
 
