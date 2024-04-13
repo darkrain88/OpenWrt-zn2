fdf73d9e67db7bc3ae2db1729a9b09bfa0c69458
darkrain88 <xiaogang.wei@gmail.com>
2024-04-13 16:04:23 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -14,11 +14,13 @@
 git clone --depth=1 https://github.com/zhengmz/luci-app-zerotier.git feeds/luci/applications/luci-app-zerotier
 
 # ddnsgo
-git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git feeds/luci/applications/ddns-go
 
 # sms-tool
 
-git clone https://github.com/4IceG/luci-app-sms-toolgit package/luci-app-sms-tool
+git clone https://github.com/4IceG/luci-app-sms-tool.git  feeds/luci/applications/luci-app-sms-tool
+
+git clone https://github.com/darkrain88/luci-app-sms-tool
 
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
@@ -27,17 +29,14 @@ git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/lu
 
 
 # MosDNS
-git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+git clone --depth=1 https://github.com/sbwml/luci-app-mosdns feeds/luci/applications/luci-app-mosdns
 
 # Adguardhome
-git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome feeds/luci/applications/luci-app-adguardhome
 # Alist
 git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
 
-# 取消主题默认设置
 
-# 修改本地时间格式
-sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
